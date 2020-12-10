54093fa9fb30727ce8a5705e7c4cb0178b5490e0
Daniel Sasse <dsasse07@gmail.com>
2020-12-10 16:49:01 -0500

    Cat class updated

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,26 @@
+require 'pry'
+require_relative 'owner'
+
 class Cat
-  # code goes here
+    attr_reader :name
+    attr_accessor :owner, :mood
+
+    @@all = []
+
+    def initialize (name_para, owner_obj)
+      @name = name_para
+      @owner = owner_obj
+      @mood = "nervous"
+      @@all << self
+    end
+
+    def self.all
+      @@all
+    end
+
 end
+
+
+danny = Owner.new
+binding.pry
+0
