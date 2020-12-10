018770e845b7183e19731d9fa51aa104ab763880
dabryant11 <74263550+dabryant11@users.noreply.github.com>
2020-12-10 13:34:15 -0800

    dog_class

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,27 @@
+require 'pry'
+
 class Dog
-  # code goes here
+
+  attr_reader :name
+  attr_accessor :owner, :mood
+
+  @@all = []
+
+  def initialize (name_para, owner_obj)
+    @name = name_para
+    @owner = owner_obj
+    @mood = "nervous"
+    @@all << self 
+  end
+  
+  def self.all
+    @@all 
+  end
+
+
 end
+
+
+
+binding.pry
+0
